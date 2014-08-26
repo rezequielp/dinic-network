a504d10761034d0e4c6075b6b090211ad38be021
rezequielp@gmail.com <rezequielp@gmail.com@949d99ae-2df4-d243-ca9c-13557de5afbb>
2014-08-26 22:34:55 +0000

    eliminados unos archivos duplicados

--- trunk/RR/compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /home/eze/discreta2_2014/dinic-network/RR/dirmain
-/usr/bin/time -a -o ../saved_test_$1.txt ./dinic -s 0 -t 1 -a < network1.txt > log1
-/usr/bin/time -a -o ../saved_test_$1.txt ./dinic -s 0 -t 1 -a < network2.txt > log2
-/usr/bin/time -a -o ../saved_test_$1.txt ./dinic -s 0 -t 1 -a < network3.txt > log3
