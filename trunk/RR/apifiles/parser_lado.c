d510420dac42baaf04715ec75176fe72056cff8a
rezequielp@gmail.com <rezequielp@gmail.com@949d99ae-2df4-d243-ca9c-13557de5afbb>
2014-08-27 17:59:15 +0000

    + doxyfile

--- trunk/RR/apifiles/parser_lado.c
+++ trunk/RR/apifiles/parser_lado.c
@@ -16,6 +16,8 @@ static bstring next_bstring(Lexer *input, int flag, const char *str);
 static bool is_theNextChar(Lexer *input, const char *ch);
 
 /** Lee todo un Lado de \a input hasta llegar a un fin de línea o de archivo.
+ *  Se asegura que se consumió input hasta el primer error o hasta completar 
+ *  el lado y No se consumió ningún "\n".
  * \param input El analizador léxico del descriptor de entrada
  * \pre input!=NULL.
  * \return Un lado bien formado si el parseo fue exitoso. \n
@@ -50,10 +52,13 @@ Lado parser_lado(Lexer *input){
 
 /** Consume el fin de línea.
  * Indica si encontro (o no) basura antes del fin de línea.
+ * Se asegura que no se consumió más entrada de la necesaria y el lexer esta 
+ * detenido justo luego de un "\n" o en el fin de archivo.
  * \param input El analizador léxico del descriptor de entrada.
  * \pre input!=NULL.
  * \return  PARSER_OK si no hay basura. \n
- *          PARSER_ERR caso contrario.*/
+ *          PARSER_ERR caso contrario.
+ */
 int parser_nextLine(Lexer *input){
     int result = PARSER_OK;     /*Si es EOF (o el sig char es '\n'), sera true*/
     bstring gbCollector = NULL; /*Recolector de caracteres basura*/
